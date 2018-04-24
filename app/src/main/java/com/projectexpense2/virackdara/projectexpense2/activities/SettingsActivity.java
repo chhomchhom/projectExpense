43509de91eee33017603492be21ea224643de520
malthibeh <30274663+malthibeh@users.noreply.github.com>
2018-04-24 04:01:52 -0400

    added
    -added add button
    -added setting activity
    -added setting menu button
    -change cardview padding
    -fixed cardview view so we can see all cards now

---
+++ app/src/main/java/com/projectexpense2/virackdara/projectexpense2/activities/SettingsActivity.java
@@ -0,0 +1,15 @@
+package com.projectexpense2.virackdara.projectexpense2.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.projectexpense2.virackdara.projectexpense2.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
--- app/src/main/java/com/projectexpense2/virackdara/projectexpense2/fragments/ExpensesFragment.java
+++ app/src/main/java/com/projectexpense2/virackdara/projectexpense2/fragments/ExpensesFragment.java
@@ -1,15 +1,22 @@
 package com.projectexpense2.virackdara.projectexpense2.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.projectexpense2.virackdara.projectexpense2.R;
+import com.projectexpense2.virackdara.projectexpense2.activities.SettingsActivity;
 import com.projectexpense2.virackdara.projectexpense2.adapters.PagerAdapter;
 import com.projectexpense2.virackdara.projectexpense2.objects.Categories;
 import com.projectexpense2.virackdara.projectexpense2.objects.ExpenseCard;
@@ -17,10 +24,17 @@ import com.projectexpense2.virackdara.projectexpense2.objects.ExpenseCard;
 import java.util.Date;
 
 public class ExpensesFragment extends Fragment{
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         View rootView = inflater.inflate(R.layout.fragment_expense, container, false);
+        Toolbar toolbar = rootView.findViewById(R.id.toolbarExpense);
+        toolbar.setTitle("");
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        setHasOptionsMenu(true);
+
         TabLayout tabLayout = rootView.findViewById(R.id.tabLayout);
         tabLayout.addTab(tabLayout.newTab().setText("Today"));
         tabLayout.addTab(tabLayout.newTab().setText("Week"));
@@ -47,18 +61,46 @@ public class ExpensesFragment extends Fragment{
 
             }
         });
+
+
+
         return rootView;
 
     }
 
     //description is optional
     //call based on if description is null or not
-    public ExpenseCard addExpense(String title, String amount, String description, Categories cat){
+    public ExpenseCard addExpense(String title, String amount, String description, Categories cat) {
         Date myDate = new Date();
-        ExpenseCard myCard = new ExpenseCard (title,amount,myDate, cat);
+        ExpenseCard myCard = new ExpenseCard(title, amount, myDate, cat);
 
         return myCard;
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_with_addbtn, menu);  // Use filter.xml from step 1
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.settings:
+                Intent myIntent = new Intent(getContext(), SettingsActivity.class);
+                getContext().startActivity(myIntent);
+                return true;
+            case R.id.item2:
+
+                // Do Fragment menu item stuff here
+                return true;
+
+            default:
+                break;
+        }
+
+        return false;
+    }
 
 }
